16dd66e9c52ccdffec319038ee789a8809d177aa
mariamrizwan01 <mariam.rizwan01@gmail.com>
2024-03-14 04:55:51 +0500

    Assignment

--- Question4.dart
+++ Question4.dart
@@ -1,10 +1,18 @@
 void main() {
-  List<int> numbers = [78, 4, 8, 23, 45];
+  List<int> numbers = [6,90,76,4,75,20]; 
+  
   int smallest = numbers[0];
   int greatest = numbers[0];
-  if (smallest > greatest) {
-    print('The smallest number : $smallest');
-  } else if (smallest < greatest) {
-    print('The greatest number : $greatest');
+
+  for (int number in numbers) {
+    if (number < smallest) {
+      smallest = number;
+    }
+    if (number > greatest) {
+      greatest = number;
+    }
   }
+
+  print('Smallest number: $smallest');
+  print('Greatest number: $greatest');
 }
